eb9ae91b5029ec9327aa2a5bfafd2b9383c31459
Yogesh070 <ythapa430@gmail.com>
2022-05-09 17:42:01 +0545

    'lastupdate'

---
+++ components/Client.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <div class="container">
+          <h2>Some of our clients</h2>
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container{
+    height: 50vh;
+    background-image: url("https://cdn.statically.io/img/axon.edu.np/f=auto%2Cq=80/wp-content/uploads/2020/02/viber_image_2020-02-27_14-42-14-270x370.jpg");
+}
+</style>
